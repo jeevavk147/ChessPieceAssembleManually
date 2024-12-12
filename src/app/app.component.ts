283365aa8bcc52a16ac59f91962c3029d332ae20
jeevavk147 <jeevavk147@gmail.com>
2024-12-12 04:58:53 -0800

    Auth and api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import awsmobile from 'src/aws-exports';
+import { Amplify } from 'aws-amplify';
+Amplify.configure(awsmobile)
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
